d0ace2ad5f1b685727e885a4279667a988b73d95
ofirdagan <ofirqd4@gmail.com>
2026-01-07 17:33:37 +0200

    nothing

--- G18_Prototype_Server/src/logic/ActionType.java
+++ G18_Prototype_Server/src/logic/ActionType.java
@@ -2,7 +2,7 @@ package logic;
 //Defines the command types sent between Client and Server.
 
 public enum ActionType {
-    READ_ORDER,      // Request to fetch a specific order by ID sdfafdasdasf
+    READ_ORDER,      // Request to fetch a specific order by ID sdfafd
     UPDATE_ORDER,    // Request to update an existing order
     GET_ALL_ORDERS,   // Request to retrieve all orders from the DB
     CLIENT_QUIT 	// Indicates that the client is requesting a graceful disconnection from the server
